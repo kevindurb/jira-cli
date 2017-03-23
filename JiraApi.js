06ccbca31c144f52274d09aa61a3c509f10c136a
Kevin Durbin <kevindurb@gmail.com>
2017-03-23 13:05:08 -0600

    Add basic api to get issue

--- JiraApi.js
+++ JiraApi.js
@@ -1,2 +1,23 @@
+const axios = require('axios');
+
 class JiraApi {
+
+  constructor(jiraDomain, authData) {
+    this.jiraDomain = jiraDomain;
+    this.authData = authData;
+
+    this.jira = axios.create({
+      baseURL: `https://${jiraDomain}.atlassian.net/rest/api/latest/`,
+      headers: {
+        'Authorization': `Basic ${authData}`,
+      },
+    });
+  }
+
+  getIssue(id = '') {
+    return this.jira.get(`issue/${id}`);
+  }
+
 }
+
+module.exports = JiraApi;
