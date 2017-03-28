5a132202aab384628e541216bd755d5a1def994b
Kevin Durbin <kevindurb@gmail.com>
2017-03-28 11:09:07 -0600

    Move auth and login to the api object for reuse

--- actions/show.js
+++ actions/show.js
@@ -1,16 +1,11 @@
 const R = require('ramda');
 const JiraApi = require('../lib/JiraApi');
-const cred = require('../lib/credentials');
-
-const login = require('../actions/login');
 
 const presentIssue = require('../presenters/issue');
 const resultData = R.propOr({}, 'data');
 
 module.exports = (issue) => (
-  cred.getCredentials()
-  .catch(login)
-  .then(JiraApi.create)
+  JiraApi.connect()
   .then((api) => {
     return api.getIssue(issue);
   })
